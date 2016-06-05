d03b8da81c1756c5c88b49ea71a6f91db6a1d7c6
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-06-05 18:01:28 +0200

    paged feeds

---
+++ assets/_plugins/feed_generator.rb
@@ -0,0 +1,40 @@
+module Jekyll
+  class PagedFeedPage < Page
+    def initialize(site, base, dir, name, page_number, pages_total, format)
+      @site = site
+      @dir = "/"
+      @name = name
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'feed.xml')
+      self.data['next'] = pages_total > page_number ? "?page=" + (page_number + 1).to_s : nil
+      self.data['last'] = pages_total > 1 ? "?page=" + pages_total.to_s : nil
+      self.data['prev'] = case page_number
+        when 1 then nil
+        when 2 then ""
+        else        "?page=" + (page_number - 1).to_s
+      end
+      self.data['myself'] = page_number == 1 ? nil : "?page=" + page_number.to_s
+      self.data['format'] = format
+    end
+  end
+
+  class PagedFeedPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      pages_total = site.posts.docs.count / site.config["episodes_per_feed_page"].ceil
+
+      site.config["episode_feed_formats"].each do |page_format|
+        name = "episodes." + page_format + ".rss"
+        page = PagedFeedPage.new(site, site.source, ".", name, 1, pages_total, page_format)
+        site.pages << page
+        (1..pages_total).each do |page_number|
+          name = "episodes." + page_format + ".rss?page=" + page_number.to_s
+          page = PagedFeedPage.new(site, site.source, ".", name, page_number, pages_total, page_format)
+          site.pages << page
+        end
+      end
+    end
+  end
+end
--- lib/jekyll/octopod/version.rb
+++ lib/jekyll/octopod/version.rb
@@ -2,8 +2,8 @@ module Jekyll
   class Octopod
     module VERSION #:nodoc:
       MAJOR = 0
-      MINOR = 5
-      TINY  = 3
+      MINOR = 6
+      TINY  = 0
 
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
