8f84e48c5bc15577cd8f955035a9e797a69925c9
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-12-07 15:55:57 +0100

    preparations for podigee webplayer

--- jekyll-octopod.gemspec
+++ jekyll-octopod.gemspec
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 $:.push File.expand_path("../lib", __FILE__)
-require "jekyll/octopod/version"
+require "octopod/version"
 
 Gem::Specification.new do |s|
   s.name = "jekyll-octopod"
@@ -19,8 +19,7 @@ Gem::Specification.new do |s|
   s.executables = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-# s.add_dependency 'byebug'
-  s.add_dependency 'jekyll', '~> 3.3.1'
+  s.add_dependency 'jekyll', '~> 3.3', '>= 3.3.1'
   s.add_dependency 'jekyll-bootflat', '~> 0.1'
   s.add_development_dependency 'rspec', '~> 2.9'
 end
---
+++ lib/jekyll/podigee_player.rb
@@ -0,0 +1,101 @@
+module Jekyll
+  module PodigeePlayer
+
+def podigee_playe
+
+#  <script>
+#    window.playerConfiguration = <%= podigee_episodestruct(@episode) %>
+#  </script>
+#  <script class="podigee-podcast-player"
+#    src="https://cdn.podigee.com/podcast-player/javascripts/podigee-podcast-player.js"
+#    data-configuration="playerConfiguration">
+#  </script>
+
+# def podigee_episodestruct(episode) do
+#   %{options: %{theme: "default",
+#                startPanel: "ChapterMarks"},
+#     extensions: %{ChapterMarks: %{},
+#                   EpisodeInfo: %{},
+#                   Playlist: %{}},
+#     title: episode.podcast.title,
+#     episode: %{media: enclosuremap(episode.enclosures),
+#                coverUrl: episode.podcast.image_url,
+#                title: episode.title,
+#                subtitle: episode.subtitle,
+#                url: episode.deep_link,
+#                description: episode.description,
+#                chaptermarks: chapterlist(episode.chapters)
+#              }
+#   }
+#   |> Poison.encode!
+#   |> raw
+# end
+
+
+# defp chapterlist(chapters) do
+#   Enum.map(chapters, fn(chapter) -> %{start: chapter.start,
+#                                       title: chapter.title} end)
+# end
+
+
+# defp enclosuremap(enclosures) do
+#   Enum.map(enclosures, fn(enclosure) -> %{filetype(enclosure) => enclosure.url} end)
+#   |> List.first
+# end
+
+    #   {{ page | podigee_player_script_tag:site }}
+    def podigee_player_script_tag(page, site)
+      return if page['audio'].nil?
+
+      options = {
+        alwaysShowHours: 'true',
+        startVolume: '0.8',
+        width: 'auto',
+        summaryVisible: 'true',
+        timecontrolsVisible: 'true',
+        chaptersVisible: 'true',
+        sharebuttonsVisible: 'true'
+      }
+
+      simple_keys = %w[]
+
+      if sitehash = site.posts.first.site.config.dup
+        sitehash.delete('title')
+        sitehash.delete('subtitle')
+        options = options.merge(sitehash)
+      end
+      options = options.merge(page)
+
+      out = "<script>\n$('audio').podlovewebplayer("
+      out << { poster: sitehash['url'] + (options['episode_cover'] || '/img/logo-360x360.png'),
+               subtitle: options['subtitle'],
+               title: options['title'],
+               alwaysShowHours: options['alwaysShowHours'],
+               startVolume: options['startVolume'],
+               width: options['width'],
+               summaryVisible: options['summaryVisible'],
+               timecontrolsVisible: options['timecontrolsVisible'],
+               chaptersVisible: options['chaptersVisible'],
+               sharebuttonsVisible: options['sharebuttonsVisible'],
+               show: { title: site['title'],
+                       subtitle: site['subtitle'],
+                       summary: site['description'],
+                       poster: sitehash['url'] + '/img/logo-360x360.png',
+                       url: sitehash['url'] },
+               chapters: options['chapters'].map {|chapter| split_chapter(chapter)},
+               downloads: [
+                 { assetTitle: options['title'],
+                   size: file_size(page['audio']['mp3']),
+                   downloadUrl: sitehash["url"] + "/episodes/" + page['audio']['mp3'] },
+               ],
+               summary: options['summary'],
+               duration: options['duration'],
+               permalink: sitehash['url'] + page['url'],
+               activeTab: "chapters"
+             }.to_json
+      out << ")\n</script>"
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::PodigeePlayer)
