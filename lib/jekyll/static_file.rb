242fa51c183821051bf6bc2f7f5da17b1f959eff
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-12-09 08:59:58 +0100

    update excluded files

--- lib/jekyll/static_file.rb
+++ lib/jekyll/static_file.rb
@@ -3,13 +3,10 @@ module Jekyll
 
     def octopod_exclude
       src = path.sub("#{@base}/", '')
-      nested_files = %w[img/bigplay.psd readme.txt
-        screenshot-1.png screenshot-2.png settings.php standalone.html
-        libs/mediaelement/README.md]
-      exclude_dirs = %w[]
+      nested_files = %w[]
 
-      excludes = %w[ChangeLog Gemfile Gemfile.lock README.md octopod]
-      return true if excludes.include?(src) || exclude_dirs.include?(File.dirname(src))
+      excludes = %w[Gemfile Gemfile.lock README.md octopod]
+      return true if excludes.include?(src))
     end
 
     alias_method :_octopod_original_write, :write
