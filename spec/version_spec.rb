e8f722ef244597165df3f56dd1679aa7e270a7c5
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-04-07 15:34:24 +0200

    bump version

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "Jekyll::Octopod::VERSION::STRING" do
-  it "should be 0.8.0" do
-    Jekyll::Octopod::VERSION::STRING.should == "0.8.0"
+  it "should be 0.8.9" do
+    Jekyll::Octopod::VERSION::STRING.should == "0.8.9"
   end
 end
