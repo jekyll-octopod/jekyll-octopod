22d0e70cf77a6ae6cec6f92c9fcb4c3257aa98de
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-06-04 14:11:26 +0200

    jsonfeeds for all attachment types

--- lib/jekyll/update_config.rb
+++ lib/jekyll/update_config.rb
@@ -5,6 +5,10 @@ module Jekyll
       site.config.update(
         'episode_feed_formats' => feeds.map { |f| f.match(/episodes\.(\w{3,4})\.rss/)[1] }
       )
+      jsonfeeds = Dir["feed.{???,????}.json"]
+      site.config.update(
+        'json_feed_formats'    => jsonfeeds.map { |f| f.match(/feed\.(\w{3,4})\.json/)[1] }
+      )
     end
   end
 end
